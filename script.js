0dd8dbd429d3dd5c8ea432bb0b221cb100280a76
Javier Ibanez <expertprep2015@gmail.com>
2021-05-10 19:37:11 -0400

    added variables with if statments for special character conditions

--- script.js
+++ script.js
@@ -25,6 +25,9 @@ var specialCharacters = [
   ".",
 ];
 
+// Array of numeric characters to be included in password
+var numericCharacters = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
+
 // Array of lowercase characters to be included in password
 var lowerCasedCharacters = [
   "a",
@@ -85,19 +88,90 @@ var upperCasedCharacters = [
   "Z",
 ];
 
+// Function to prompt user for password options
+function getPasswordOptions() {
+  // Variable to store length of password from user input
+  var length = parseInt(
+    prompt("How many characters would you like your password to contain?")
+  );
 
-// Array of numeric characters to be included in password
-var numericCharacters = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
+  // Conditional statement to check if password length is a number. Prompts end if this evaluates false
+  if (isNaN(length) === true) {
+    alert("Password length must be provided as a number");
+    return;
+  }
 
-// Assignment Code
-var generateBtn = document.querySelector("#generate");
+ // Conditional statement to check if password length is at least 8 characters long. Prompts end if this evaluates false
+ if (length < 8) {
+  alert("Password length must be at least 8 characters");
+  return;
+}
 
+// Conditional statement to check if password length is less than 128 characters long. Prompts end if this evaluates false
+if (length > 128) {
+  alert("Password length must less than 129 characters");
+  return;
+}
+
+// Variable to store boolean regarding the inclusion of special characters
+var hasSpecialCharacters = confirm(
+  "Click OK to confirm including special characters."
+);
+
+// Variable to store boolean regarding the inclusion of numeric characters
+var hasNumericCharacters = confirm(
+  "Click OK to confirm including numeric characters."
+);
+
+// Variable to store boolean regarding the inclusion of lowercase characters
+var hasLowerCasedCharacters = confirm(
+  "Click OK to confirm including lowercase characters."
+);
+
+// Variable to store boolean regarding the inclusion of uppercase characters
+var hasUpperCasedCharacters = confirm(
+  "Click OK to confirm including uppercase characters."
+);
+
+ // Conditional statement to check if user does not include any types of characters. Password generator ends if all four variables evaluate to false
+ if (
+  hasSpecialCharacters === false &&
+  hasNumericCharacters === false &&
+  hasLowerCasedCharacters === false &&
+  hasUpperCasedCharacters === false
+) {
+  alert("Must select at least one character type");
+  return;
+}
+
+// Object to store user input
+var passwordOptions = {
+  length: length,
+  hasSpecialCharacters: hasSpecialCharacters,
+  hasNumericCharacters: hasNumericCharacters,
+  hasLowerCasedCharacters: hasLowerCasedCharacters,
+  hasUpperCasedCharacters: hasUpperCasedCharacters,
+};
+
+return passwordOptions;
+}
+
+// Function for getting a random element from an array
+function getRandom(arr) {
+var randIndex = Math.floor(Math.random() * arr.length);
+var randElement = arr[randIndex];
+
+return randElement;
+}
+
+// Function to generate password with user input
 function generatePassword() {
 
-  var testAlert = prompt("How many characters would you like your password to contain?")
-
-  return testAlert
+ 
 }
+
+// Get references to the #generate element
+var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
 function writePassword() {
